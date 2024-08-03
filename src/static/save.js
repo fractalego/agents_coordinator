4eb617e69fcf9201cf5084927c3abf7f55797ec2
Alberto Cetoli <albertocetoli@gmail.com>
2024-08-03 12:46:34 +0100

    reverting to old save method

--- src/static/save.js
+++ src/static/save.js
@@ -2,20 +2,11 @@ $(document).ready(function() {
     $('#save-button').click(function() {
     var pageContent = $('html').clone(); // Clone the HTML content
 
-    // Ensure all dynamic values are included
     pageContent.find('textarea').each(function() {
         $(this).text($(this).val()); // Update textareas with their current values
     });
-
-
-
-    // You may need to include dynamically created jQuery functions
-    // This is often handled by ensuring the JavaScript code is included in the saved page
-
-    // Create a Blob with the serialized content
     var blob = new Blob([pageContent.html()], { type: 'text/html' });
 
-    // Create a download link and trigger the download
     var link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
     link.download = 'savedPage.html';
