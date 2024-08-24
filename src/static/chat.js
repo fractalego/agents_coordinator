036755bb66709c778e4e11169f93ee0794982207
Alberto Cetoli <albertocetoli@gmail.com>
2024-08-24 09:30:01 +0100

    added connections for start and chat

--- src/static/chat.js
+++ src/static/chat.js
@@ -1,7 +1,64 @@
 $(function () {
-    $("#chat").draggable({
-        drag: function () {
-            jQuery('#start').connections('update');
+    $('#chat').draggable({
+            start: function(event, ui) {
+                // Only start dragging if the item is selected
+                if (!$(this).hasClass('ui-selected')) {
+                    $(".draggable").removeClass('ui-selected');
+                    $(this).addClass('ui-selected');
+                }
+
+                // Save the initial position of each selected item
+                var startPosition = ui.position;
+                $(".ui-selected").each(function() {
+                    var $this = $(this);
+                    var itemPosition = $this.position();
+                    $this.data("startPos", itemPosition);
+                    $this.data("offset", {
+                        top: itemPosition.top - startPosition.top,
+                        left: itemPosition.left - startPosition.left
+                    });
+                });
+            },
+            drag: function (event, ui) {
+                // Move each selected item according to the initial offset
+                $(".ui-selected").not(this).each(function() {
+                    var $this = $(this);
+                    var offset = $this.data("offset");
+                    $this.css({
+                        top: ui.position.top + offset.top,
+                        left: ui.position.left + offset.left
+                    });
+                });
+                jQuery('#chat').connections('update');
+            },
+            stop: function (event, ui) {
+                $(".container").removeClass("ui-selected");
+            }
+        });
+        $('#chat').resizable({
+            resize: function () {
+                jQuery('#chat').connections('update');
+            }
+        });
+
+
+    $('#chat').on('click', function (event) {
+        var random_index = Math.floor(Math.random() * 1000000);
+        var connection_name = `connection-${random_index}`;
+        console.log('#chat');
+        if (cntrlIsPressed && current_selection != null) {
+            $(current_selection).connections({
+                to: '#chat',
+                'class': `my-connection ${connection_name}`
+            });
+            connection_list_name_from_and_to.push([`${connection_name}`, current_selection, '#chat']);
+            $(`.${connection_name}`).attr('tabindex', '-1');
+            $(`.${connection_name}`).on('keydown', function (event) {
+                console.log('#${connection_name}');
+                $('#chat').connections('remove');
+            });
         }
+        current_selection = null;
+
     });
 });
--- src/static/start.js
+++ src/static/start.js
@@ -8,4 +8,22 @@ $(function () {
     $("#start").on('click', function (event) {
         current_selection = '#start';
     });
+
+    $('#start').on('click', function (event) {
+        console.log('#${new_container_name}');
+        if (cntrlIsPressed && current_selection != null) {
+            // $(current_selection).connections({
+            //     to: '#start',
+            //     'class': 'my-connection ${connection_name}'
+            // });
+            // connection_list_name_from_and_to.push(['${connection_name}', current_selection, '#start']);
+            // $('.${connection_name}').attr('tabindex', '-1');
+            // $('.${connection_name}').on('keydown', function (event) {
+            //     console.log('#${connection_name}');
+            //     $('#${new_container_name}').connections('remove');
+            // });
+            current_selection = null;
+        }
+        current_selection = '#start';
+    });
 });
